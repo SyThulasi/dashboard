cbd6db27265991ef7aa11044391a8aebfe46e24f
Yogeswaran Thulasiyan <sythulasi.6@gmail.com>
2023-05-14 00:28:23 +0530

    create a dash board

--- app.py
+++ app.py
@@ -2,8 +2,40 @@ from h2o_wave import Q, ui, app, main
 
 @app('/displayData')
 
-async def serve(q: Q):
-    q.page['card1'] = ui.form_card(box='1 1 2 2 ', items=[
-        ui.text(content = 'Hellow, World'),
-    ])
+async def server(q: Q):
+    
+    apply_layout(q)
+    show_homepage(q)
+
     await q.page.save()
+
+def apply_layout(q:Q):
+    q.page['meta'] = ui.meta_card(
+        box='', 
+        theme='nord', 
+        layouts=[
+            ui.layout(
+                breakpoint='xl',
+                width='1600px',
+                zones=[
+                    ui.zone('header'),
+                    ui.zone('footer')
+                ]
+            )
+        ]
+    )
+
+
+def show_homepage(q: Q):
+    q.page['header'] = ui.header_card(
+        box = ui.box('header', 
+        width='100%', 
+        height='86px'),
+        icon='Money',
+        icon_color='Black',
+        title='Paris Housing Market',
+        subtitle='This is an imaginary housing dataset')
+
+    q.page['footer'] = ui.footer_card(
+        box='footer', 
+        caption='This dataset was obtained from [Kaggle](https://www.kaggle.com/datasets/mssmartypants/paris-housing-price-prediction)')
